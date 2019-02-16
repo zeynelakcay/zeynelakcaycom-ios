74014ddfc3829435809fe91777a98b77eaa1a0d6
zeynelakcay <zeynelakcay61@gmail.com>
2019-02-16 21:09:17 +0300

    Initial Commit

---
+++ zeynelakcaycom-ios/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  zeynelakcaycom-ios
+//
+//  Created by Zeynel Akçay on 16.02.2019.
+//  Copyright © 2019 Zeynel Akçay. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ zeynelakcaycom-ios/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  zeynelakcaycom-ios
+//
+//  Created by Zeynel Akçay on 16.02.2019.
+//  Copyright © 2019 Zeynel Akçay. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
