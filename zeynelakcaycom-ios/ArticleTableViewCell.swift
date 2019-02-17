c3e0d663a1e0a5256818b8ed4c0a8c9462f029fa
zeynelakcay <zeynelakcay61@gmail.com>
2019-02-17 19:03:42 +0300

    #8 table view geliştirmesi yapıldı

---
+++ zeynelakcaycom-ios/ArticleTableViewCell.swift
@@ -0,0 +1,29 @@
+//
+//  ArticleTableViewCell.swift
+//  zeynelakcaycom-ios
+//
+//  Created by Zeynel Akçay on 17.02.2019.
+//  Copyright © 2019 Zeynel Akçay. All rights reserved.
+//
+
+import UIKit
+
+class ArticleTableViewCell: UITableViewCell {
+    @IBOutlet weak var foodImageView: UIImageView!
+    @IBOutlet weak var foodName: UILabel!
+    
+    @IBOutlet weak var locationStore: UILabel!
+    @IBOutlet weak var priceFood: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
